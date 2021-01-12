d0db002aee57c01cf957c61a248ac2183a786783
Mitch Stark <mitch.stark@tekton.com>
2021-01-11 20:51:30 -0500

    began board development

---
+++ src/board/Space.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import SpaceTypeEnum from './SpaceTypeEnum';
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * Math.floor(max));
+}
+
+
+function getSpaceType() {
+    const spaceTypeValue = getRandomInt(100);
+    let spaceType = SpaceTypeEnum.GRASS;
+
+    if (spaceTypeValue >= 50 && spaceTypeValue < 65) {
+        spaceType = SpaceTypeEnum.MOUNTAIN;
+    } else if (spaceTypeValue >= 65 && spaceTypeValue < 80) {
+        spaceType = SpaceTypeEnum.FOREST;
+    } else if (spaceTypeValue >= 80 && spaceTypeValue < 90) {
+        spaceType = SpaceTypeEnum.DESERT;
+    } else if (spaceTypeValue >= 90) {
+        spaceType = SpaceTypeEnum.WATER;
+    }
+
+    return spaceType;
+}
+
+export default function Space() {
+    const [spaceType] = React.useState(getSpaceType);
+
+    return (
+        <div className={'space ' + spaceType}>
+            <h1>Test</h1>
+        </div>
+    );
+}
---
+++ src/board/SpaceTypeEnum.js
@@ -0,0 +1,22 @@
+export default {
+    'GRASS': {
+        id: 1,
+        class: 'grass'
+    },
+    'FOREST': {
+        id: 2,
+        class: 'forest'
+    },
+    'MOUNTAIN': {
+        id: 3,
+        class: 'mountain'
+    },
+    'DESERT': {
+        id: 4,
+        class: 'desert'
+    },
+    'WATER': {
+        id: 5,
+        class: 'water'
+    }
+};
